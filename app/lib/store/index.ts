249389bc9680d5aac8513bdef282a3a8ba441c72
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2025-11-04 13:58:02 -0300

    delete unwanted stores

--- app/lib/store/index.ts
+++ app/lib/store/index.ts
@@ -1,5 +1,2 @@
 // Export all stores from a single entry point
-export { useEventStore } from './eventStore'
-export { useBookingStore } from './bookingStore'
-export { useUserStore } from './userStore'
-
+export { useUserStore } from "./userStore";
